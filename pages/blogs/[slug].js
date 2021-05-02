fce5c5778e08eb8d37f0761c747879c03f76c448
Sangeet <sangeet.dhakal@gmail.com>
2021-05-02 20:21:38 +0545

    image loader add

--- pages/blogs/[slug].js
+++ pages/blogs/[slug].js
@@ -81,9 +81,9 @@ const SingleBlog = ({ blog}) => {
         );
     };
 
-    // const myLoader=({src,width,quality})=>{
-    //     return (`${API}/blog/photo/${blog.slug}`)
-    //     }
+    const myLoader=({src,width,quality})=>{
+        return (`${API}/blog/photo/${blog.slug}`)
+        }
 
     return (
         <React.Fragment>
@@ -95,7 +95,7 @@ const SingleBlog = ({ blog}) => {
                             <section>
                                 <div className="col-md-12" style={{ marginTop: '0px' }}>
                                     <Image
-                                        // loader={myLoader}
+                                        loader={myLoader}
                                         src={`${API}/blog/photo/${blog.slug}`}
                                         alt={blog.title}
                                         height={500}
