d5f3488e27cedc57a3517e702517bb2ae93bc97e
SangeetDhakal <65996703+SangeetDhakal@users.noreply.github.com>
2021-05-05 12:04:36 +0545

    editor crash change

---
+++ helpers/quill.js
@@ -0,0 +1,35 @@
+export const QuillModules = {
+    clipboard:{},
+    toolbar: [
+        
+        [{ header: '2' }, { header: [3, 4, 5, 6] }, { font: [] }],
+        [{ size: [] }],
+        ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+        [{ list: 'ordered' }, { list: 'bullet' }],
+        ['link', 'image', 'video'],
+        ['clean'],
+        ['code-block'],
+        
+        
+    ]
+};
+
+export const QuillFormats = [
+    
+    'header',
+    'font',
+    'size',
+    'bold',
+    'italic',
+    'underline',
+    'strike',
+    'blockquote',
+    'list',
+    'bullet',
+    'link',
+    'image',
+    'video',
+    'code-block',
+    
+    
+];
