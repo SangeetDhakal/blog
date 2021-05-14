099058917aca02c4a54dc10768fec116229f9a42
SangeetDhakal <65996703+SangeetDhakal@users.noreply.github.com>
2021-05-14 16:15:57 +0545

    twitter

--- pages/_document.js
+++ pages/_document.js
@@ -29,7 +29,7 @@ class MyDocument extends Document {
           <meta name='application-name' content='Wiki Stacks' />
           <meta name='apple-mobile-web-app-capable' content='yes' />
           <meta name='apple-mobile-web-app-status-bar-style' content='default' />
-          <meta name='apple-mobile-web-app-title' content='PWA App' />
+          <meta name='apple-mobile-web-app-title' content='Wiki Stacks' />
           <meta name='description' content='Wiki, News & Entertainment related to your favourite celebs!!!' />
           <meta name='format-detection' content='telephone=no' />
           <meta name='mobile-web-app-capable' content='yes' />
@@ -50,13 +50,6 @@ class MyDocument extends Document {
           <link rel='shortcut icon' href='/favicon.ico' />
           <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto:300,400,500' />
 
-          
-          <meta property='og:type' content='website' />
-          <meta property='og:title' content='Wiki Stacks' />
-          <meta property='og:description' content='Wiki, News & Entertainment related to your favourite celebs!!!' />
-          <meta property='og:site_name' content='Wiki Stacks' />
-          <meta property='og:url' content='https://wikistacks.com' />
-          <meta property='og:image' content='https://wikistacks.com/Static/icons/apple-touch-icon.png' />
           <meta charSet="UTF-8" />
           <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover' />
 
